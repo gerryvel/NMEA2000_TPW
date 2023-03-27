66fee543d04952dd7f2863d5ac3244f00fc9c255
Gerry <gerry@sebb.de>
2023-03-27 19:47:25 +0200

    Update configuration.h

--- Abraxas_TP/configuration.h
+++ Abraxas_TP/configuration.h
@@ -1,3 +1,5 @@
+#ifndef __configuration__H__
+#define __configuration__H__
 
 // Configuration N2k
 #define ESP32_CAN_TX_PIN GPIO_NUM_5  // Set CAN TX port to 5 
@@ -8,37 +10,48 @@
 #define PAGE_REFRESH 10 
 
 //Configuration AP 
-const char* HostName       = "NMEA2000TPW";
-const char* AP_SSID        = "NMEA2000TPW";  // SSID Name
-const char* AP_PASSWORD    = "12345678";    // SSID Password - Set to NULL to have an open AP
+#define HostName        "NMEA2000TPW"
+#define AP_SSID         "NMEA2000TPW"  // SSID Name
+#define AP_PASSWORD     "12345678"    // SSID Password - Set to NULL to have an open AP
 const int   channel        = 10;                // WiFi Channel number between 1 and 13
 const bool  hide_SSID      = false;             // To disable SSID broadcast -> SSID will not appear in a basic WiFi scan
 const int   max_connection = 2;                 // Maximum simultaneous connected clients on the AP
 
+// Variables for WIFI-AP
+IPAddress IP = IPAddress(192, 168, 15, 1);
+IPAddress Gateway = IPAddress(192, 168, 15, 1);
+IPAddress NMask = IPAddress(255, 255, 255, 0);
+IPAddress AP_IP;
+IPAddress CL_IP;
+IPAddress SELF_IP;
+
 //Configuration Client (Network Data Windsensor)
-#define CL_SSID      "NoWa"					
-#define CL_PASSWORD  "12345678"				
+#define CL_SSID      "NoWa"					//"ABRAXAS"
+#define CL_PASSWORD  "12345678"				//"schmilka200"
 int iSTA_on = 0;                            // Status STA-Mode
 int bConnect_CL = 0;
 bool bClientConnected = 0;
 
-//Confuration Sensors
-#define BMP_STA 21
-#define BMP_SCL 22
-float bmp280_temperature = 0;
-float bmp280_pressure = 0;
-float bmp280_altitude = 0;
+//Confuration Sensors bmp
+#define BMP_SDA 21                      //Standard 21
+#define BMP_SCL 22                      //Standard 22
+#define SEALEVELPRESSURE_HPA (1013.25)  //1013.25
+float fbmp_temperature = 0;
+float fbmp_pressure = 0;
+float fbmp_altitude = 0;
+String sBMP_Status = "";
 
 //Definiton NMEA0183 MWV
-double MWV_WindDirectionT = 0;
-double MWV_WindSpeedM = 0;
-double VWR_WindDirectionM = 0;
-double VWR_WindAngle = 0;
-double VWR_WindSpeedkn = 0;
-double VWR_WindSpeedms = 0;
+double dMWV_WindDirectionT = 0;
+double dMWV_WindSpeedM = 0;
+double dVWR_WindDirectionM = 0;
+double dVWR_WindAngle = 0;
+double dVWR_WindSpeedkn = 0;
+double dVWR_WindSpeedms = 0;
 
 //Configuration NMEA0183
 #define SERVER_HOST_NAME "192.168.4.1"		//"192.168.76.34"
 #define TCP_PORT 6666						//6666
 #define DNS_PORT 53
 
+#endif  
