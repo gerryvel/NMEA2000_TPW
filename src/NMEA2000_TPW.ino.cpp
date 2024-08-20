c5489e2d283df87a34041c3a9b8f26171dfe7a6f
gerryvel <gerry@sebb.de>
2024-08-20 21:47:03 +0200

    V2.2

--- src/NMEA2000_TPW.ino.cpp
+++ src/NMEA2000_TPW.ino.cpp
@@ -1,27 +1,7 @@
-/*
-  This code is free software; you can redistribute it and/or
-  modify it under the terms of the GNU Lesser General Public
-  License as published by the Free Software Foundation; either
-  version 2.1 of the License, or (at your option) any later version.
-  This code is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-  Lesser General Public License for more details.
-  You should have received a copy of the GNU Lesser General Public
-  License along with this library; if not, write to the Free Software
-  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-
-  NMEA2000 Temperature and Barometric Pressure with BMP280.
-  Reads messages from NMEA0183 WindSensor and forwards them to the N2k bus.
-  The messages, which will be handled has been defined on NMEA0183Handlers.cpp
-  on NMEA0183Handlers variable initialization. So this does not automatically
-  handle all NMEA0183 messages. If there is no handler for some message you need,
-  you have to write handler for it and add it to the NMEA0183Handlers variable
-  initialization. NMEA0183.h add ParseTCPMessage function from Andras Szap.
-
-  // Version 2.0, 24.06.2024, gerryvel Gerry Sebb
-*/
-
+# 1 "C:\\Users\\GERRYA~1\\AppData\\Local\\Temp\\tmp4rgat7rx"
+#include <Arduino.h>
+# 1 "C:/Users/gerryadmin/Documents/PlatformIO/Projects/NMEA2000_TPW/src/NMEA2000_TPW.ino"
+# 25 "C:/Users/gerryadmin/Documents/PlatformIO/Projects/NMEA2000_TPW/src/NMEA2000_TPW.ino"
 #include <Arduino.h>
 #include "BoardInfo.h"
 #include "configuration.h"
@@ -36,33 +16,41 @@
 #include <LittleFS.h>
 #include <ESPmDNS.h>
 #include <WiFiUdp.h>
-#include <ArduinoOTA.h> 
+#include <ArduinoOTA.h>
 #include "web.h"
 
-// N2k
+
 #include <N2kMsg.h>
 #include <NMEA2000.h>
 #include <N2kMessages.h>
 
-// NMEA0183
+
 #include "NMEA0183.h"
 #include <NMEA0183Msg.h>
 #include <NMEA0183Messages.h>
 
-// Can Bus
-#include <NMEA2000_CAN.h>  // This will automatically choose right CAN library and create suitable NMEA2000 object
 
-// BMP 388
+#include <NMEA2000_CAN.h>
+
+
 Adafruit_BMP3XX bmp;
 
-// NMEA 0183 Stream
+
 WiFiClient nmeaclient;
-char buffer[128]; // Puffer zur Speicherung der empfangenen Daten
+char buffer[128];
 int bufferIndex = 0;
 
 static ETSTimer intervalTimer;
-
-/***************************** Funktionen **************************/
+void read_nmea0183();
+bool IsTimeToUpdate(unsigned long NextUpdate);
+void SetNextUpdate(unsigned long & NextUpdate, unsigned long Period);
+void CheckSourceAddressChange();
+void SendN2kWind(void);
+void SendN2kTemperatur(void);
+void SendN2kPressure(void);
+void setup();
+void loop();
+#line 66 "C:/Users/gerryadmin/Documents/PlatformIO/Projects/NMEA2000_TPW/src/NMEA2000_TPW.ino"
 void read_nmea0183()
 {
   if (nmeaclient.connected()) {
@@ -71,17 +59,17 @@ void read_nmea0183()
       Serial.write(c);
 
       if (c == '\n' || bufferIndex >= sizeof(buffer) - 1) {
-        buffer[bufferIndex] = '\0'; // Abschluss der Zeichenkette
+        buffer[bufferIndex] = '\0';
 
-        // Überprüfen, ob es sich um eine VWR-Nachricht handelt
+
         if (strstr(buffer, "VWR") != NULL) {
-          // Beispiel: "$IIVWR,090.0,R,005.5,N,002.8,M,009.0,K*50"
-       
+
+
           float windDirection = 0.0;
           float windSpeed = 0.0;
           sscanf(buffer, "$%*[^,],%f,R,%f,N", &windDirection, &windSpeed);
 
-          // Speichern Daten in Variablen
+
           Serial.print("Windrichtung: ");
           Serial.println(windDirection);
           Serial.print("Windgeschwindigkeit: ");
@@ -90,7 +78,7 @@ void read_nmea0183()
           dVWR_WindSpeedkn = windSpeed;
         }
 
-        // Puffer zurücksetzen
+
         bufferIndex = 0;
       } else {
         buffer[bufferIndex++] = c;
@@ -100,24 +88,24 @@ void read_nmea0183()
     Serial.println("Verbindung zum NMEA-Server verloren");
     nmeaclient.stop();
 
-    // Versuchen, die Verbindung wiederherzustellen
+
     if (nmeaclient.connect(SERVER_HOST_NAME, TCP_PORT)) {
       Serial.println("Verbunden mit NMEA-Server");
     } else {
       Serial.println("Verbindung zum NMEA-Server fehlgeschlagen");
     }
   }
-  
+
 }
 
-String NMEA_Info; // formatted Output NMEA2000-Message
-int NodeAddress = 0; // To store last Node Address
-Preferences preferences; // Nonvolatile storage on ESP32 - To store LastDeviceAddress
-// Set the information for other bus devices, which messages we support
-const unsigned long TransmitMessages[] PROGMEM = { 130310L, // Outside Environmental parameters
-                                                   130312L, // Temperature
-                                                   130314L, // Pressure
-                                                   130306L, // Wind
+String NMEA_Info;
+int NodeAddress = 0;
+Preferences preferences;
+
+const unsigned long TransmitMessages[] PROGMEM = { 130310L,
+                                                   130312L,
+                                                   130314L,
+                                                   130306L,
                                                    0
                                                  };
 
@@ -137,8 +125,8 @@ void SetNextUpdate(unsigned long & NextUpdate, unsigned long Period){
 void CheckSourceAddressChange() {
   int SourceAddress = NMEA2000.GetN2kSource();
 
-  if (SourceAddress != NodeAddress) { // Save potentially changed Source Address to NVS memory
-    NodeAddress = SourceAddress;      // Set new Node Address (to save only once)
+  if (SourceAddress != NodeAddress) {
+    NodeAddress = SourceAddress;
     preferences.begin("nvs", false);
     preferences.putInt("LastNodeAddress", SourceAddress);
     preferences.end();
@@ -151,7 +139,7 @@ void SendN2kWind(void){
   tN2kMsg N2kMsg;
 
   if (IsTimeToUpdate(SlowDataUpdated)){
-    SetNextUpdate(SlowDataUpdated, SlowDataUpdatePeriod);      
+    SetNextUpdate(SlowDataUpdated, SlowDataUpdatePeriod);
     Serial.print("N2k Wind Data: ");
     Serial.printf("VWR Windrichtung: %f ° - VWR Windgeschw: %f kn\n", dVWR_WindDirectionM, dVWR_WindSpeedkn);
 
@@ -193,64 +181,64 @@ void SendN2kPressure(void){
   }
 }
 
-//================================== SETUP ==============================//
+
 void setup()
 {
   Serial.begin(115200);
 
   Serial.printf("TPW Sensor setup %s start\n", Version);
 
-//Filesystem
-	if (!LittleFS.begin(true)) {
-		Serial.println("An Error has occurred while mounting LittleFS");
-		return;
-	}
-	Serial.println("Speicher LittleFS benutzt:");
-	Serial.println(LittleFS.usedBytes());
-	File root = LittleFS.open("/");
+
+ if (!LittleFS.begin(true)) {
+  Serial.println("An Error has occurred while mounting LittleFS");
+  return;
+ }
+ Serial.println("Speicher LittleFS benutzt:");
+ Serial.println(LittleFS.usedBytes());
+ File root = LittleFS.open("/");
   listDir(LittleFS, "/", 3);
   readConfig("/config.json");
-	CL_SSID = tAP_Config.wAP_SSID;
-	CL_PASSWORD = tAP_Config.wAP_Password;
+ CL_SSID = tAP_Config.wAP_SSID;
+ CL_PASSWORD = tAP_Config.wAP_Password;
   Serial.println("Client SSID: " + CL_SSID + " , Passwort: " + CL_PASSWORD);
 
   freeHeapSpace();
 
-// I2C
+
   Wire.begin(BMP_SDA, BMP_SCL);
   I2C_scan();
-//LED
+
   LEDInit();
   LEDoff();
-// Boardinfo	
+
   sBoardInfo = boardInfo.ShowChipIDtoString();
 
-//Wifi
+
   WiFi.mode(WIFI_AP_STA);
   WiFi.softAPdisconnect();
   if(WiFi.softAP(AP_SSID, AP_PASSWORD, channel, hide_SSID, max_connection)){
     WiFi.softAPConfig(IP, Gateway, NMask);
     Serial.println("");
     Serial.println("Network " + String(AP_SSID) + " running");
-//    Serial.println("AP IP address: " + WiFi.softAPIP());
+
     delay(1000);
   } else {
       Serial.println("Starting AP failed.");
-      digitalWrite(LED(Red), HIGH);  
-      delay(1000); 
+      digitalWrite(LED(Red), HIGH);
+      delay(1000);
       ESP.restart();
   }
-  
+
   WiFi.hostname(HostName);
   Serial.println("Set Hostname done");
 
 WiFiDiag();
 
-// Anmelden mit WiFi als Client an Windmesser
+
 Serial.println("Client Connection");
   WiFi.disconnect(true);
-  delay(1000);   
-  int count = 0; // Init Counter WFIConnect  
+  delay(1000);
+  int count = 0;
 WiFi.begin(CL_SSID, CL_PASSWORD);
   while (WiFi.status() != WL_CONNECTED)
   {
@@ -267,18 +255,18 @@ WiFi.begin(CL_SSID, CL_PASSWORD);
   }
   else
     Serial.println("Client Connection failed");
-    WiFi.reconnect(); 
+    WiFi.reconnect();
 
-// Start OTA
+
   ArduinoOTA
     .onStart([]() {
       String type;
       if (ArduinoOTA.getCommand() == U_FLASH)
         type = "sketch";
-      else // U_SPIFFS
+      else
         type = "filesystem";
 
-      // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
+
       Serial.println("Start updating " + type);
     })
     .onEnd([]() {
@@ -298,25 +286,25 @@ WiFi.begin(CL_SSID, CL_PASSWORD);
 
   ArduinoOTA.begin();
 
-// Start TCP (HTTP) server
-	server.begin();
-	Serial.println("TCP server started");
 
-// Mit dem NMEA-Server verbinden
+ server.begin();
+ Serial.println("TCP server started");
+
+
   if (nmeaclient.connect(SERVER_HOST_NAME, TCP_PORT)) {
     Serial.println("Verbunden mit NMEA-Server");
   } else {
     Serial.println("Verbindung zum NMEA-Server fehlgeschlagen");
   }
 
-// BMP38x begin & setup
+
   if (!bmp.begin_I2C())
   {
     Serial.println("Could not find a valid BMP3 sensor, check wiring!");
   }
     bmp.setTemperatureOversampling(BMP3_OVERSAMPLING_8X);
     bmp.setPressureOversampling(BMP3_OVERSAMPLING_4X);
-    bmp.setIIRFilterCoeff(BMP3_IIR_FILTER_COEFF_3); 
+    bmp.setIIRFilterCoeff(BMP3_IIR_FILTER_COEFF_3);
 
   MDNSResponder mdns;
   MDNS.begin(HostName);
@@ -324,30 +312,30 @@ WiFi.begin(CL_SSID, CL_PASSWORD);
 
   website();
 
-  //***********************************************Setup NMEA2000 system*******************************************
-  // Reserve enough buffer for sending all messages. This does not work on small memory devices like Uno or Mega
+
+
   NMEA2000.SetN2kCANMsgBufSize(8);
   NMEA2000.SetN2kCANReceiveFrameBufSize(250);
   NMEA2000.SetN2kCANSendFrameBufSize(250);
 
-  NMEA2000.SetProductInformation("TPW01", // Manufacturer's Model serial code
-                                 107, // Manufacturer's product code
-                                 "TPW Sensor Module",  // Manufacturer's Model ID
-                                 "2.1.0.0 (2024-07-06)",  // Manufacturer's Software version code
-                                 "2.0.0.0 (2023-01-26)" // Manufacturer's Model version
+  NMEA2000.SetProductInformation("TPW01",
+                                 107,
+                                 "TPW Sensor Module",
+                                 "2.1.0.0 (2024-07-06)",
+                                 "2.0.0.0 (2023-01-26)"
                                 );
-  // Set device information
-  NMEA2000.SetDeviceInformation(8, // Unique number. Use e.g. Serial number.
-                                130, // Device function=PC Gateway. See codes on http://www.nmea.org/Assets/20120726%20nmea%202000%20class%20%26%20function%20codes%20v%202.00.pdf
-                                25, // Device class=Inter/Intranetwork Device. See codes on http://www.nmea.org/Assets/20120726%20nmea%202000%20class%20%26%20function%20codes%20v%202.00.pdf
-                                2046 // Just choosen free from code list on http://www.nmea.org/Assets/20121020%20nmea%202000%20registration%20list.pdf
+
+  NMEA2000.SetDeviceInformation(8,
+                                130,
+                                25,
+                                2046
                                );
 
-  // If you also want to see all traffic on the bus use N2km_ListenAndNode instead of N2km_NodeOnly below
-  NMEA2000.SetForwardType(tNMEA2000::fwdt_Text); // Show in clear text. Leave uncommented for default Actisense format.
 
-  preferences.begin("nvs", false);                          // Open nonvolatile storage (nvs)
-  NodeAddress = preferences.getInt("LastNodeAddress", 34);  // Read stored last NodeAddress, default 34
+  NMEA2000.SetForwardType(tNMEA2000::fwdt_Text);
+
+  preferences.begin("nvs", false);
+  NodeAddress = preferences.getInt("LastNodeAddress", 34);
   preferences.end();
   Serial.printf("NodeAddress=%d\n", NodeAddress);
 
@@ -359,20 +347,20 @@ WiFi.begin(CL_SSID, CL_PASSWORD);
     Serial.println(" NMEA2000 Initialized");
   else
     Serial.println(" NMEA2000 Initialized failed");
-  
+
 }
-//=================================== Loop ==============================//
+
 void loop()
-{ 
+{
   LEDoff();
-  // Listen NMEA0183 or reconnect Wifi
+
   bConnect_CL = WiFi.status() == WL_CONNECTED ? 1 : 0;
   Serial.print("WL Connection Status: ");
   Serial.println(WL_CONNECTED);
   Serial.print("bConnect Client: ");
   Serial.println(bConnect_CL);
-  { // Listen NMEA0183
-    if (bConnect_CL == 1){ // Connected an listen
+  {
+    if (bConnect_CL == 1){
       Serial.printf("Wifi %s connencted!\n", CL_SSID);
       read_nmea0183();
       delay(100);
@@ -381,7 +369,7 @@ void loop()
       Serial.println("Wifi connect failed!\n");
       bConnect_CL = 0;
       Serial.printf("Reconnecting to %s\n", CL_SSID);
-      WiFi.reconnect();    // wifi down, reconnect here
+      WiFi.reconnect();
       delay(500);
       int WLcount = 0;
      int UpCount = 0;
@@ -389,7 +377,7 @@ void loop()
         delay(50);
         Serial.printf(".");
         LEDflash(LED(Red));
-        if (UpCount >= 20)  // just keep terminal from scrolling sideways
+        if (UpCount >= 20)
         {
           UpCount = 0;
           Serial.printf("\n");
@@ -401,26 +389,26 @@ void loop()
     }
   }
 
- { // LED visu Wifi
+ {
     switch (WiFi.status()){
-      case 0: LEDblink(LED(Blue)); break;  // WL_IDLE_STATUS
-      case 3: LEDblink(LED(Green)); break;  // WL_CONNECTED
-      case 4:                              // WL_CONNECT_FAILED
-      case 5:                              // WL_CONNECTION_LOST
-      case 6: LEDblink(LED(Red)); break;  // WL_DISCONNECTED
+      case 0: LEDblink(LED(Blue)); break;
+      case 3: LEDblink(LED(Green)); break;
+      case 4:
+      case 5:
+      case 6: LEDblink(LED(Red)); break;
       default: LEDoff();
     }
   }
 
 ArduinoOTA.handle();
 
-// Status AP 
+
   Serial.printf("Soft-AP IP address: %s\n", WiFi.softAPIP().toString());
   sCL_Status = sWifiStatus(WiFi.status());
 
  delay(500);
 
- //Read BMP
+
      if (!bmp.performReading()) {
       Serial.println("Lesefehler BMP");
       sBMP_Status = "BMP Lesefehler";
@@ -437,7 +425,7 @@ ArduinoOTA.handle();
       delay(100);
     }
 
-  //N2K
+
     SendN2kWind();
     SendN2kPressure();
     SendN2kTemperatur();
